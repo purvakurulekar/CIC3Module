7e97a126d3f751eedd6b6d26ab98541f52b13343
purvakurulekar <purva.deshmukh@2020spaces.com>
2022-04-19 09:53:45 +0530

    push to master

---
+++ clientapi1.js
@@ -0,0 +1,9 @@
+var supertest = require('supertest')
+const request = supertest('https://cic-catalogs-dev-app.azurewebsites.net/api/catalogs?lang=en-US')
+
+it('test case1', async done =>{
+
+    const response = await request.get('/api/catalogs')
+    console.log(response.body)
+    done()
+})
